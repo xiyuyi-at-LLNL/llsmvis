d08caf2bf817607c0f2850cb8160eee38f9bea4f
xiyu <yi10@llnl.gov>
2021-09-26 19:35:28 -0700

    added MP4 generation function from group parsers

--- llsmvis/utils.py
+++ llsmvis/utils.py
@@ -8,6 +8,13 @@ from skimage import io
 import numpy as np
 import os
 import skimage.external.tifffile
+import sys
+sys.path.append("/g/g92/yi10") # add more paths below when needed
+from llsmvis import *
+from llsmvis import utils
+import skvideo.io
+import warnings; warnings.simplefilter('ignore')
+
 
 #import pandas as pd
 
@@ -36,12 +43,14 @@ def render_mpl_table(p, data, col_width=3.0, row_height=0.625, font_size=16,
     fig.savefig(p.fpath+'/results_dsk/'+p.fname_head + '_deskewed/MyRep_data_specifics.png')
     return [fig, ax]
 
+
 def wpad2square(imraw):
     # pad the imraw images (1 channel) into squares
     m = np.max(imraw.shape)
     impad = np.ones((m, m)) * np.max(imraw.ravel())
     impad[0:imraw.shape[0], 0:imraw.shape[1]] = imraw
     return impad
+
 
 def frameXYmip(im, imname, reldT, absdT, savepath, lower=0, upper=1, data_info='None'):
     # this function is to generate a single frame of the labeled MIP movie in the XY plane.
@@ -73,7 +82,7 @@ def getLabeledXYmip_MP4(p, lb, ub, reference_frame_index = 0, channel_inds=None)
     t0=datetime.strptime(p.acquisition_startT,'%m/%d/%Y %I:%M:%S %p')
     # loop over all t steps for all channels
     if channel_inds is None:
-        channel_inds=np.arange(0, len(p.channel_names))
+        channel_inds=np.arange(0,len(p.channel_names))
 
     for cind in channel_inds:
         c=p.channel_names[cind]
@@ -125,4 +134,116 @@ def getLabeledXYmip_MP4(p, lb, ub, reference_frame_index = 0, channel_inds=None)
         writer.close()
     return None
 
+
+def getLabeledXYmip_MP4_fromGparser(g, lb, ub, reference_frame_index=0, channel_inds=None):
+    # p is the group parser
+    # this function can only be used after deskew is finished and all the mip stacks are available.
+    ptlist = gp.time_sorted_indiv_parsers  # a list of parsers with time order
+    # take out the initial date time from the first parser
+    t0 = datetime.strptime(ptlist[0].acquisition_startT, '%m/%d/%Y %I:%M:%S %p')
+
+    # loop over all t steps for all channels
+    if channel_inds is None:
+        channel_inds = np.arange(0, len(ptlist[0].channel_names))
+
+    for cind in channel_inds:  # loop over all the channels
+        c = ptlist[0].channel_names[cind]
+        # find the correct file name for the MIP project in XY of this channel.
+        fname0 = ptlist[0].fpath + '/results_dsk/' + ptlist[0].fname_head + '_deskewed' + '/MIP_' + c + '_XY.tif'
+
+        # Create a folder for the mp4 video
+        try:
+            os.mkdir(ptlist[0].fpath + '/mp4s')
+        except:
+            pass
+        vidp = ptlist[0].fpath + '/mp4s/' + ptlist[0].fname_head + '_deskewed'
+
+        try:
+            os.mkdir(vidp)
+        except:
+            pass
+
+        info = ptlist[0].fname_head + ',    channel: ' + c
+
+        # Initiate an mp4 file.
+        writer = skvideo.io.FFmpegWriter(vidp + '/MyRep_MIP_' + c + '_wLabels.mp4', outputdict={"-pix_fmt": "yuv420p"})
+        print(fname0)
+
+        # Read in the image of the first time point.
+        try:
+            imstack = skimage.external.tifffile.imread(fname0).astype('float')
+        except IOError:
+            print('failed to open file: \n' + fname)
+
+        # Find the peak and the base of the image at the first time point.
+        peak = np.max(imstack.ravel());
+        base = np.min(imstack.ravel());
+        imstack = (imstack - base) / (peak - base)
+        print('maximum is:' + str(np.max(imstack.ravel())))
+
+        # Create a temp folder to hold all .png files as movie frames.
+        f = vidp + '/mp4s_' + c
+        try:
+            os.mkdir(f)
+        except:
+            pass
+
+        # Print the image shape
+        print(imstack.shape)
+        stackN = len(ptlist)
+        for i in np.arange(0, stackN):  # Loop over all tsteps
+            # Prepare the correct parameters
+            for t in ptlist[i].dict_tiffs:
+                if t['channel index'] == cind:
+                    tc = t
+                    dt = tc['time (datetime)'] - t0
+                    secondsstr = '{:05.2f}'.format(np.float(str(dt).split(':')[2]))
+                    reldTstr = ':'.join(str(dt).split(':')[:2]) + ':' + secondsstr
+                    absdTstr = tc['time (datetime)'].strftime('%m/%d/%Y %I:%M:%S %p')
+
+            # Find the correct file name for the MIP project in XY of this channel.
+            fnamei = ptlist[i].fpath + '/results_dsk/' + ptlist[i].fname_head + '_deskewed' + '/MIP_' + c + '_XY.tif'
+
+            # Load this image
+            try:
+                imstack = skimage.external.tifffile.imread(fnamei).astype('float')
+            except IOError:
+                print('failed to open file: \n' + fname)
+
+            # Find the peak and the base of the image at the first time point.
+            peak = np.max(imstack.ravel());
+            base = np.min(imstack.ravel());
+            imstack = (imstack - base) / (peak - base)
+
+            # Frame this mip image with correct labels, save as png.
+            frameXYmip(im=imstack, imname=str(i) + '.png', reldT=reldTstr, \
+                       absdT=absdTstr, savepath=f, lower=lb, upper=ub, data_info=info)
+
+        # Loop over all frames again and export as .mp4
+        for i in np.arange(0, stackN):  # Loop over all tsteps again
+            im = skimage.io.imread(f + '/' + str(i) + '.png')
+            writer.writeFrame(im)
+
+        writer.close()
+    return None
+
+
+def deskew_a_tiff_stack(fpath, fname_head):
+    """
+
+    :param fpath: the path string to the tiff stack
+    :param fname_head: the fname_head of the tiff stack
+    :return: none. The deskewd tiff will be stored at the designated results folder.
+    """
+    # get a parser:
+    p = LLSMParser(fpath, fname_head)
+    # define the deskewer
+    d = Deskewer(p)
+    # deskew all the tiffs
+    d.deskew_all_tiffs()
+    [fig, ax] = utils.render_mpl_table(p, p.data_properties, header_columns=0, col_width=7.0)
+
+
+def datetime_to_integer(t):
+    return np.int(1e10*t.year + 1e8*t.month + t.day + 1e6*t.hour + 1e4*t.minute + 1e2*t.second)
 
