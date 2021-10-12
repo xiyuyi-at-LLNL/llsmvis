1a0ce6f56f72eb3e619b5979eab65369cc1239f8
xiyu <yi10@llnl.gov>
2021-10-12 16:36:59 -0700

    added init for hp3d extension

--- llsmvis/extensions/hp3d/__init__.py
+++ llsmvis/extensions/hp3d/__init__.py
@@ -1,4 +1,18 @@
-from llsmvis.extensions.hp3d import croproi3d
-from llsmvis.extensions.hp3d import selectroi
-from llsmvis.extensions.hp3d import masscenter
+try:
+    from llsmvis.extensions.hp3d import croproi3d
+except:
+    print('extensions.hp3d module: importing croproi3d failed')
+    pass
 
+
+try:
+    from llsmvis.extensions.hp3d import selectroi
+except:
+    print('extensions.hp3d module: importing selectroi failed')
+    pass
+
+try:
+    from llsmvis.extensions.hp3d import masscenter
+except:
+    print('extensions.hp3d module: importing masscenter failed')
+    pass
