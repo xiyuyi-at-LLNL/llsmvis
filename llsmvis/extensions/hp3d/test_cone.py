8bec046973c5f54eb35a90842ece61d5c3bdde98
Haichao <haichao.miao1@gmail.com>
2021-12-29 16:58:32 -0800

    added some sample code for cone code

--- llsmvis/extensions/hp3d/test_cone.py
+++ llsmvis/extensions/hp3d/test_cone.py
@@ -12,3 +12,13 @@ cone_protrusion_prop = sm.get_volume_surface_area(r'C:\Users\miao1\Data\h3pd\con
 print('volume of cone and protrusion', cone_protrusion_prop[0])
 print('surface area protrusion', cut_protrusion_prop[1])
 #print('intersection surface area and volume', sm.calc_morphometrics(r'C:\Users\miao1\Data\h3pd\cone test\intersect.stl'))
+
+sm.get_cut(r'C:\Users\miao1\Data\h3pd\cone test 3\cell.stl', r'C:\Users\miao1\Data\h3pd\cone test 3\cone_1.stl', r'C:\Users\miao1\Data\h3pd\cone test 3\cut_cell.stl', r'C:\Users\miao1\Data\h3pd\cone test 3\cut_protrusion.stl', r'C:\Users\miao1\Data\h3pd\cone test 3\cone_protrusion.stl')
+
+cut_protrusion_prop = sm.get_volume_surface_area(r'C:\Users\miao1\Data\h3pd\cone test 2\cut_protrusion.stl')
+cone_protrusion_prop = sm.get_volume_surface_area(r'C:\Users\miao1\Data\h3pd\cone test 2\cone_protrusion.stl')
+
+print('volume of cone and protrusion', cone_protrusion_prop[0])
+print('surface area protrusion', cut_protrusion_prop[1])
+#print('intersection surface area and volume', sm.calc_morphometrics(r'C:\Users\miao1\Data\h3pd\cone test\intersect.stl'))
+
